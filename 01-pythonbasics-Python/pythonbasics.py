7221c5c3c7d6e0f0174d66fe9b6e742c32ec5e9d
voruganti96 <55683406+voruganti96@users.noreply.github.com>
2020-07-14 11:01:08 +0530

    Jul 14  11:01:08

--- 01-pythonbasics-Python/pythonbasics.py
+++ 01-pythonbasics-Python/pythonbasics.py
@@ -4,8 +4,10 @@
 # Return the string with "return".
 # You can only have the string once in your code.
 # Don't just copy/paste it 5 times into a single variable!
+
+
 def show_excitement():
     # Your code goes here!
-    return "I am super excited for this course! " *5 
+    return "I am super excited for this course! " * 5 
 
 
