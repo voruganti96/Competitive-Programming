0824d3adeefef1cc40c76507f25023bf35027599
voruganti96 <55683406+voruganti96@users.noreply.github.com>
2020-07-18 22:02:34 +0530

    Jul 18  22:02:33

--- 15-isrighttriangle-Python/isrighttriangle.py
+++ 15-isrighttriangle-Python/isrighttriangle.py
@@ -7,4 +7,19 @@
 
 def isrighttriangle(x1, y1, x2, y2, x3, y3):
 	# your code goes here
-	pass
+	a = (y2-y1) / (x2-x1)
+
+	b = (y3-y2) / (x3 - x2)
+
+	c = (y3-y1) / (x3-x1)
+
+	if (a * b == -1):
+		return True
+	elif( b *c == -1):
+		return True
+	elif( a * c == -1):
+		return True
+	else:
+		return False
+	
+
