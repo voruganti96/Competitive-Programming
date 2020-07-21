c120d6bbdc543949d9588efaaaf75650f2b8bb35
voruganti96 <55683406+voruganti96@users.noreply.github.com>
2020-07-21 20:33:51 +0530

    Jul 21  20:33:51

--- 02-nth_happy_prime-Python/nth_happy_prime.py
+++ 02-nth_happy_prime-Python/nth_happy_prime.py
@@ -4,5 +4,41 @@
 
 
 
+
+
+def ishappynumber(n):
+	if n ==1:
+		return True
+	elif n <= 0 or n ==4:
+		return False
+
+	else:
+		s = 0
+		rem = 0
+		while ( n > 0):
+			rem = n %  10
+			s = s + rem()**2
+			n = n//10
+
+		return ishappynumber(s)
+
+
+def isprime(n):
+	flag = 0
+	for i in range(1, n+1,1):
+		if n % i ==0:
+			flag = flag +1
+	if flag ==2:
+			return True
+	else:
+			return False
+
+
 def fun_nth_happy_prime(n):
-	return 0
+	count = 1
+	while n >= 0:
+		if ishappynumber(count) and isprime(count):
+			n = n-1
+			count = count +1
+
+	return count -1
