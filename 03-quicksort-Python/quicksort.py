9be9cb724bea4a03772d47484df540cc5b7a6de5
voruganti96 <55683406+voruganti96@users.noreply.github.com>
2020-07-21 23:30:17 +0530

    Jul 21  23:30:13

--- 03-quicksort-Python/quicksort.py
+++ 03-quicksort-Python/quicksort.py
@@ -11,4 +11,21 @@ def qsort(array, start, end):
 
 		part = partition(array, start, end)
 		qsort(array, start , part-1)
-		qsor
+		qsort(array, part+1 , end)
+
+
+def partition(array, start, end):
+	pivot = start+1
+	high = end
+	while True:
+		while low <= high and array[high] >= pivot:
+			high = high -1
+		while low <= high and array[low] <= pivot:
+			low = low+1
+		if low <= high:
+			array[low],array[high],array[low]
+		else:
+			break
+
+	array[start] , array[high] = array[high] , array[start]
+	return high
