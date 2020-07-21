dcc18a426672e070eefaa2a0add9230152de0bee
voruganti96 <55683406+voruganti96@users.noreply.github.com>
2020-07-21 15:01:51 +0530

    Jul 21  15:01:51

--- 01-find_int_roots-Python/find_int_roots.py
+++ 01-find_int_roots-Python/find_int_roots.py
@@ -10,6 +10,13 @@ import math
 def fun_find_int_roots(a, b, c):
 
 	x = ( -b + math.sqrt((b**2) - 4 * a * c))/ 2*a
-	y = ( -b + math.sqrt((b**2) - 4 * a * c))/ 2*a
+	y = ( -b - math.sqrt((b**2) - 4 * a * c))/ 2*a
 
+	if ( x >y):
+		return y,x
+	else:
+		return x,y
+
+
+# ZeroDivisionError
 
