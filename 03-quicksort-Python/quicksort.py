104d1b3f2f31fb376c07ac42af8c527c257e0144
voruganti96 <55683406+voruganti96@users.noreply.github.com>
2020-07-21 23:27:11 +0530

    Jul 21  23:27:06

--- 03-quicksort-Python/quicksort.py
+++ 03-quicksort-Python/quicksort.py
@@ -4,3 +4,11 @@ Output a sorted list."""
 def quicksort(array):
 	# Your code goes here
 	qsort(array,0,len(array)-1)
+	return array
+
+def qsort(array, start, end):
+	if start < end:
+
+		part = partition(array, start, end)
+		qsort(array, start , part-1)
+		qsor
