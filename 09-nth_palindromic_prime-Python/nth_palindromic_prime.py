76ae58288c35c18eccfee32c4238ce4f0df23ee6
voruganti96 <55683406+voruganti96@users.noreply.github.com>
2020-07-22 19:51:42 +0530

    Jul 22  19:51:41

--- 09-nth_palindromic_prime-Python/nth_palindromic_prime.py
+++ 09-nth_palindromic_prime-Python/nth_palindromic_prime.py
@@ -5,6 +5,34 @@
 
 
 
+def isprime(n):
+	if n <=1:
+		return False
+	for i in range(2,(n//2)+1):
+		if n%i ==0:
+			return False
+	return True
+
+
+def ispalindrome(n):
+	s = str(n)
+	a = s[::-1]
+
+	if int(a) == n:
+		return True
+	else:
+		return False
+
+
+
+
+
+
 
 def fun_nth_palindromic_prime(n):
-	return 0
+	count = 1
+	while n >=0:
+		if ispalindrome(count) and isprime(count):
+			n = n-1
+		count = count +1
+	return count-1
