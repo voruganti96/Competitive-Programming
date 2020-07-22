5f9728edbb2ae7c6735ac56b457bca47b3e4394c
voruganti96 <55683406+voruganti96@users.noreply.github.com>
2020-07-22 23:41:12 +0530

    Jul 22  23:41:12

--- 08-multipolynomials-Python/multipolynomials.py
+++ 08-multipolynomials-Python/multipolynomials.py
@@ -11,7 +11,8 @@ def multipolynomials(p1, p2):
 	# Your code goes here
 	p1=np.poly1d(p1)
 	p2=np.poly1d(p2)
-	print("P1:",p1,"\nP2:",p2)
+	print("P1:",p1)
+	print("P2:",p2)
 	#return convolve(p1,p2)
 	
 p1=(2,0,3)
