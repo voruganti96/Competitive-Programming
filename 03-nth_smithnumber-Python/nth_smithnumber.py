eddf72ef4cdbfa9b4c93153ef7379ef77b3e8f96
voruganti96 <55683406+voruganti96@users.noreply.github.com>
2020-07-24 05:22:57 +0530

    Jul 24  05:22:55

--- 03-nth_smithnumber-Python/nth_smithnumber.py
+++ 03-nth_smithnumber-Python/nth_smithnumber.py
@@ -8,6 +8,8 @@
 # so fun_nthsmithnumber(1) should return 22
 
 
+all_factors = []
+
 def fun_nth_smithnumber(n):
     present_number =0
     starting_number = 3
@@ -15,3 +17,49 @@ def fun_nth_smithnumber(n):
     while( present_number <= n):
         starting_number +=1
         all_factors.clear()
+
+        if(verify(starting_number)):
+            present_number += 1
+    return starting_number
+
+
+def verify(num):
+    if(is_prime(num)):
+        return False
+    for i in range(2,num):
+        if(is_prime(i) and num % i ==0):
+            print(i)
+            all_factors.append(i)
+
+            if (num / i == float(i)):
+                all_factors.append(i)
+
+    result = 0
+
+    for i in all_factors:
+        while( i !=0):
+            result += i% 10
+            i = i //10
+
+    num_result = 0
+    while( num !=0):
+        num_result += num %10
+        num = num //10
+
+    if(result == num_result):
+        return True
+    else:
+        return False
+
+
+all_primes = [2]
+
+def is_prime(num):
+    if(num in all_primes):
+        return True
+    for j in range(2,num):
+        if (num % j ==0):
+            return False
+    all_primes.append(num)
+    return True
+
