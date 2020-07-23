d54246b9bcd47224461e24564d073210e82081cd
voruganti96 <55683406+voruganti96@users.noreply.github.com>
2020-07-24 04:26:57 +0530

    Jul 24  04:26:57

--- 01-matrixmultiply-Python/matrixmultiply.py
+++ 01-matrixmultiply-Python/matrixmultiply.py
@@ -8,6 +8,22 @@ def fun_matrixmultiply(m1, m2):
     for i in m1:
         if len(i) !=len(m2):
             return None
+    
+
+    result = []
+
+
+    for i in range(len(m1)):
+        l = []
+        for j in range(len(m2[0])):
+            x = 0
+            for k in range (len(m2)):
+                x += m1[i][k] * m2[k][j]
+
+                l.append(x)
+
+        result.append(l)
+
         
 
 
