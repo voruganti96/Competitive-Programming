e1c746344290a3cbcd8092a14216d0145a1e51ce
voruganti96 <55683406+voruganti96@users.noreply.github.com>
2020-07-25 00:22:05 +0530

    Jul 25  00:22:04

--- 07-samechars-Python/samechars.py
+++ 07-samechars-Python/samechars.py
@@ -8,7 +8,7 @@
 def samechars(s1, s2):
 	# Your code goes here
 	s1=sorted(list(set(p1)))
-	print("Sorted keys")
+	print("Sorted keys of firest string:",)
 	s2=sorted(list(set(p2)))
 	if s1 == s2:
 		return True
