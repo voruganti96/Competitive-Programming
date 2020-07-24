e523cd27a601e7762dc43784237144937059d4c7
voruganti96 <55683406+voruganti96@users.noreply.github.com>
2020-07-25 00:28:28 +0530

    Jul 25  00:28:28

--- 07-samechars-Python/samechars.py
+++ 07-samechars-Python/samechars.py
@@ -5,14 +5,14 @@
 # case-sensitive, so "ABC" and "abc" do not contain the same characters. The function returns False if either 
 # parameter is not a string, but returns True if both strings are empty (why?).
 
-def samechars(s1, s2):
+def samechars(s1, p2):
 	
-	s1=sorted(list(set(p1)))
+	p1=sorted(list(set(s1)))
 	#print("Sorted keys of 1st string:",s1)
 	s2=sorted(list(set(p2)))
 	#print("Sorted keys of 2nd string:",s1)
-	
+
 	if s1 == s2:
 		print("==sAmE_cHaRs==")
 		return True
-		
+samechars("abcabcabc", "abc")
