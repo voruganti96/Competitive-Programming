9cb60651b0606e6d20d108f03ffbfe3bc39beba9
voruganti96 <55683406+voruganti96@users.noreply.github.com>
2020-07-24 15:22:39 +0530

    Jul 24  15:22:39

--- 01-nth_kaprekarnumber-Python/nth_kaprekarnumber.py
+++ 01-nth_kaprekarnumber-Python/nth_kaprekarnumber.py
@@ -21,4 +21,5 @@ def fun_nth_kaprekarnumber(n):
         power = str(power)
 
         for j in range(1,len(power)):
-            left = power
+            left = power[:j]
+            right = int(power[j:])
