94a53428cd72f03bcc0cee76f22cf5835a9412b3
voruganti96 <55683406+voruganti96@users.noreply.github.com>
2020-07-24 21:26:03 +0530

    Jul 24  21:26:01

--- 05-happyprimes-Python/happyprimes.py
+++ 05-happyprimes-Python/happyprimes.py
@@ -43,9 +43,10 @@ def ishappyprimenumber(n):
 def nthHappyNumber(n):
     happy_list = []
     x=0
-    for i in range(1,10000):
+    for i in range(1,1000):
         if ishappynumber(i) == True: happy_list.append(i)
     #print("Happy Numbers List:\n",happy_list)
+    if n >= len(happy_list):return None
     x+=(happy_list[n-1])
     #print(n,'th','Happy Number =>',x)
     return x
