b8266222b848e86065cf325977f408e966ba86d8
voruganti96 <55683406+voruganti96@users.noreply.github.com>
2020-07-24 15:43:16 +0530

    Jul 24  15:43:15

--- 06-sumofsquaresofdigits-Python/sumofsquaresofdigit.py
+++ 06-sumofsquaresofdigits-Python/sumofsquaresofdigit.py
@@ -8,6 +8,6 @@ def sumofsquaresofdigit(n):
 	# Your code goes here
 	string = str(n)
 	for i in range(0,len(string)):
-		print(string[i])
+		print((int(string[i]))**2)
 	
 sumofsquaresofdigit(67567000)
