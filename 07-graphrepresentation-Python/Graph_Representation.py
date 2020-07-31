e6474da1297770ef119a937ca384bda37f95304a
voruganti96 <55683406+voruganti96@users.noreply.github.com>
2020-08-01 02:34:22 +0530

    Aug 01  02:34:18

--- 07-graphrepresentation-Python/Graph_Representation.py
+++ 07-graphrepresentation-Python/Graph_Representation.py
@@ -62,7 +62,7 @@ class Graph(object):
         
         for node in self.nodes:
             if(node.value != edge.node_to.value):
-                adjacency_matrix
+                adjacency_matrix[edge.node]
 
 
 
