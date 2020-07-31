4d8f215e768e7ce459dc0867c32a6268c08ba53f
voruganti96 <55683406+voruganti96@users.noreply.github.com>
2020-07-31 23:09:23 +0530

    Jul 31  23:09:20

--- 07-nth_powerfulnumber-Python/nthpowerfulnumber.py
+++ 07-nth_powerfulnumber-Python/nthpowerfulnumber.py
@@ -1,48 +1,36 @@
-# nthPowerfulNumber(n) [20 pts]
-# Write the function nthPowerfulNumber(n). See here for details. So nthPowerfulNumber(0) returns 
-# 1, and nthPowerfulNumber(10) returns 64.
-# A number n is said to be Powerful Number if for every prime factor p of it, p2 also divides it. 
-# For example:- 36 is a powerful number. It is divisible by both 3 and square of 3 i.e, 9.
+def primefactors(p):
+	list1 = []
+	while(p%2 == 0):
+		list1.append(2)
+		p = p//2
+	for i in range(3, p//2+1, 2):
+		while(p%i == 0):
+			list1.append(i)
+			p = p//i
+	if(p>2):
+		list1.append(p)
+	return list1
+
+print(primefactors(36))
+
+def powerfull(n):
+	checklist = primefactors(n)
+	for i in checklist:
+		if(n % i**2 != 0):
+			return False
+	return True
 
 
 def nthpowerfulnumber(n):
 	# Your code goes here
-	count = 0
-	num = 1
-	while(count <=n):
-		if(ispowerful(num)):
-			count +=1
-		
-		num +=1
+	val = 1
+	count = -1
+	while(count < n):
+		if(powerfull(val)):
+			count += 1
+			if(count == n):
+				return val
+		val += 1
 
-	return (num-1)
-
-def ispowerful(n):
-	l = []
-	lst = []
-	count = 0
-	if( n ==1):
-		return n
-
-	for i in range( n//2 +1):
-		n1= 1
-		c = 0
-		while(i >= n1):
-			if( i %n1 ==0):
-				c+=1
-			n1 += 1
-
-		if(c ==1 or c==2):
-			lst.append(i)
-
-	for i in range(len(lst)):
-		if(n%lst[i] ==0):
-			count +=1
-			if(n%lst[i]**2 ==0):
-				l.append(lst[i])
-
-	if( len(l) == count and len(l) >l):
-		return True
-
-	return False
-
+	
+print(nthpowerfulnumber(4))
