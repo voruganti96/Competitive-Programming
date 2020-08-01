e9866128ce9fb75972865497f5afa4599ff4330b
voruganti96 <55683406+voruganti96@users.noreply.github.com>
2020-08-02 00:29:44 +0530

    Aug 02  00:29:44

--- 08-multipolynomials-Python/multipolynomials.py
+++ 08-multipolynomials-Python/multipolynomials.py
@@ -4,20 +4,22 @@
 # the product of the two. For example, multiplyPolynomials([2,0,3], [4,5]) represents the problem (2x**2 + 3)(4x + 
 # 5), and:    (2x**2 + 3)(4x + 5) = 8x**3 + 10x**2 + 12x + 15
 # And so this returns the list [8, 10, 12, 15].
-from numpy.polynomial import polynomial as poly
-import numpy as np
+# from numpy.polynomial import polynomial as poly
+# import numpy as np
 
 def multipolynomials(p1, p2):
 	# Your code goes here
-	p1=np.poly1d(p1)
-	p2=np.poly1d(p2)
-	print("P1:\n",p1)
-	print("P2:\n",p2)
-	res_poly = poly.polymul(p1,p2)
-	int_res=[]
-	for i in res_poly:
-		int_res.append(int(i))
-	return int_res
+	# p1=np.poly1d(p1)
+	# p2=np.poly1d(p2)
+	# print("P1:\n",p1)
+	# print("P2:\n",p2)
+	# res_poly = poly.polymul(p1,p2)
+	# int_res=[]
+	# for i in res_poly:
+	# 	int_res.append(int(i))
+	# return int_res
+
+	m = len
 
 	
 
