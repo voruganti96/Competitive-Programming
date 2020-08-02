a22108224a9a07b8955a3b035525778b68728e43
voruganti96 <55683406+voruganti96@users.noreply.github.com>
2020-08-02 11:55:17 +0530

    matrix multiply

--- 01-matrixmultiply-Python/matrixmultiply.py
+++ 01-matrixmultiply-Python/matrixmultiply.py
@@ -48,9 +48,11 @@ def fun_matrixmultiply(m1, m2):
         mat.append(mati)
 
     for i in range(0,len(m1)):
-        for k in range(0,len(m2[0])):
+        for j in range(0,len(m2[0])):
             for k in range(0,len(m2)):
                 mat[i][j] += m1[i][k] * m2[k][j]
+
+    return mat
 
 
 
